157688c8b622b4a0c354dc88546f1101c222a9a9
bmlistener <bmlistener@gmail.com>
2024-03-19 18:46:41 +0800

    create project

---
+++ src/main/java/com/matrix/mynoteusers/MynoteUsersApplication.java
@@ -0,0 +1,13 @@
+package com.matrix.mynoteusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynoteUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MynoteUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matrix/mynoteusers/MynoteUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matrix.mynoteusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynoteUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
